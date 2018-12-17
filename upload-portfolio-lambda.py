2751e17acb6f282d26a9512491ac357359a151d3
SydneyN <ssnettesheim@gmail.com>
2018-12-17 13:23:05 -0700

    Lambda Compatable

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,16 +4,19 @@ import io
 from io import StringIO
 import zipfile
 
-s3 = boto3.resource('s3') # , config=Config(signature_version='s4v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3') # , config=Config(signature_version='s4v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.sydneynettesheim.com')
-build_bucket = s3.Bucket('portfoliobuild.sydneynettesheim.com')
+    portfolio_bucket = s3.Bucket('portfolio.sydneynettesheim.com')
+    build_bucket = s3.Bucket('portfoliobuild.sydneynettesheim.com')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hello from Lambda'
